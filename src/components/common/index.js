526e35816060b5a8d6538d5cf6875c62c6773f92
adityaexe <kmradi2002@gmail.com>
2025-11-08 13:54:51 +0530

    chore: centralize exports for common UI components
    - Added barrel file to re-export ErrorBoundary, PageHeader, StatusBadge,
      EmptyState, LoadingWrapper, and ErrorAlert
    - Simplifies imports and improves project structure

---
+++ src/components/common/index.js
@@ -0,0 +1,10 @@
+/**
+ * Central export for common components
+ */
+export { default as ErrorBoundary } from './ErrorBoundary';
+export { default as PageHeader } from './PageHeader';
+export { default as StatusBadge } from './StatusBadge';
+export { default as EmptyState } from './EmptyState';
+export { default as LoadingWrapper } from './LoadingWrapper';
+export { default as ErrorAlert } from './ErrorAlert';
+
